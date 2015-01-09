73736b2aebd7089413055204f0086425490f0457
Johannes Bazer <johannes.bazer@gmail.com>
2015-01-09 15:02:20 +0100

    Initial Project

---
+++ desktop/src/de/jbazer/survivalgame/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package de.jbazer.survivalgame.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import de.jbazer.survivalgame.MySurvivalGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MySurvivalGame(), config);
+	}
+}
